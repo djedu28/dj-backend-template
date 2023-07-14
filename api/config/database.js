e419446a6d45b1fb390b5dfea4181283305d181f
Eduardo Santos <43933482+djedu28@users.noreply.github.com>
2023-07-14 04:52:09 -0300

    api com BD PG no sequelize funcionando na vercel

--- api/config/database.js
+++ api/config/database.js
@@ -1,25 +1,50 @@
 require('dotenv/config');
+const pg = require('pg');
 
-module.exports = process.env.BD !== "PROD"
-    ? { // DEV
-        dialect: 'sqlite',
-        storage: './database.sqlite', // caminho para o arquivo do banco de dados
-        define: {
-            timestamps: true,
-            underscored: true,
-        },
-    }
-    : { // PROD
-        dialect: 'postgres',
-        host: process.env.BD_HOST || 'motty.db.elephantsql.com',
-        options: {
-            port: process.env.BD_PORT || '5432'
-        },
-        username: process.env.BD_USER,
-        password: process.env.BD_PASS,
-        database: process.env.BD_DATA,
-        define: {
-            timestamps: true,
-            underscored: true,
-        },
-    }
+module.exports =
+
+    process.env.BD === "PROD" ?
+        { // PROD
+            dialect: 'postgres',
+            host: process.env.BD_HOST || 'motty.db.elephantsql.com',
+            options: {
+                port: process.env.BD_PORT || '5432'
+            },
+            username: process.env.BD_USER,
+            password: process.env.BD_PASS,
+            database: process.env.BD_DATA,
+            define: {
+                timestamps: true,
+                underscored: true,
+            },
+        }
+        : process.env.BD === "VERCEL" ?
+            {
+                dialect: 'postgres',
+                dialectModule: pg,
+                host: process.env.POSTGRES_HOST,
+                options: {
+                    port: process.env.POSTGRES_PORT || '5432',
+                },
+                username: process.env.POSTGRES_USER,
+                password: process.env.POSTGRES_PASSWORD,
+                database: process.env.POSTGRES_DATABASE,
+                define: {
+                    timestamps: true,
+                    underscored: true,
+                },
+                dialectOptions: {
+                    ssl: {
+                        require: true,
+                        rejectUnauthorized: false
+                    }
+                }
+            }
+            : { // DEV
+                dialect: 'sqlite',
+                storage: 'api/database/database.sqlite', // caminho para o arquivo do banco de dados
+                define: {
+                    timestamps: true,
+                    underscored: true,
+                },
+            }
